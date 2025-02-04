972238064142d145c4e79d5debcd8c1316dd29ab
Paul <paul@qa.com>
2025-02-04 14:18:39 +0000

    name changes

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -10,9 +10,9 @@ public class App
     {
         System.out.println(sayHello());
         System.out.println(sayHelloToSomeone("Alice"));
-        System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
-        System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Frank"));
+        System.out.println(sayHelloToSomeone("Stella"));
+        System.out.println(sayHelloToSomeone("PaulW"));
         System.out.println(sayGoodbye());
     }
 
